883cad48b4cb14a2125476815a0b868abb4b03ef
meiyi <meiyi@DESKTOP-759F2PU>
2026-04-12 00:20:00 +0800

    添加taiwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
+import tailwindcss from '@tailwindcss/vite';
 import houdini from "houdini/vite";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [houdini(), sveltekit()]
-});
+export default defineConfig({ plugins: [tailwindcss(), houdini(), sveltekit()] });
