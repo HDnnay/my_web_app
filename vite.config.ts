c505c3887310357e6e805f3de6b3b839bfc3ab6d
meiyi <meiyi@DESKTOP-759F2PU>
2026-04-20 21:16:49 +0800

    add

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({ 
     plugins: [tailwindcss(),houdini(),  sveltekit()] ,
+	server: {
+        port: 5174,      // 修改为你想要的端口号
+        strictPort: false, // 若设为 true，端口被占用时程序会报错而非自动切换
+    },
     resolve: {
 			alias: {
 				$houdini: './$houdini',
